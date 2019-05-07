e155d30c91f75e69c5b884c10eb7207080098866
Silas Walker <sw099y@att.com>
2019-05-07 12:01:44 -0700

    Added practice directory and practice_server python file

---
+++ practice/practice_server.py
@@ -0,0 +1,28 @@
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write(self.path[1:].encode())
+    def do_POST(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        self.wfile.write("Hello world, HTTP!\n".encode())
+        self.wfile.write(self.path[1:].encode())
+    
+if __name__ == '__main__':
+    server_address = ('', 8000)  # Serve on all addresses, port 8000.
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
